41a457cdb70a49c56fb26488db5402459d632a98
Luquier <nancy.tsr1@gmail.com>
2016-11-25 21:38:55 +0900

    move camera sequencer
    MoveCamera(x,y,z)

---
+++ Assets/Scripts/SequencerCommandMoveCamera.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using PixelCrushers.DialogueSystem;
+using PlayMaker;
+
+namespace PixelCrushers.DialogueSystem.SequencerCommands
+{
+
+    public class SequencerCommandMoveCamera : SequencerCommand
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+            var x = float.Parse(GetParameter(0));
+            var y = float.Parse(GetParameter(1));
+            var z = float.Parse(GetParameter(2));
+            Camera.main.gameObject.transform.position = new Vector3(x, y, z);
+            Stop();
+        }
+    }
+}
